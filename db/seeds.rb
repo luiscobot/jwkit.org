150073e0f5f5c55c286e153b6082c1a2554a6bfb
Luis Romero <dev@luisromero.co>
2023-11-16 00:45:54 -0500

    add database seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,30 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+FieldServiceGroup.create(number: 101)
+FieldServiceGroup.create(number: 202)
+FieldServiceGroup.create(number: 303)
+
+PRIVILEGE_OF_SERVICE = [
+  "auxiliary pioneer",
+  "regular pioneer",
+  "special pioneer",
+  "field missionary"
+]
+
+publishers = [
+  { name: "Kenneth Cook, Jr." },
+  { name: "Gage Fleegle" },
+  { name: "Samuel Herd" },
+  { name: "Geoffrey Jackson" },
+  { name: "Stephen Lett" },
+  { name: "Gerrit Lösch" },
+  { name: "Mark Sanderson" },
+  { name: "David Splane" },
+  { name: "Jeffrey Winder" }
+]
+
+publishers.each do |publisher|
+  Publisher.create(name: publisher[:name], privilege_of_service: [PRIVILEGE_OF_SERVICE.sample, nil].sample, field_service_group: FieldServiceGroup.all.sample)
+end
