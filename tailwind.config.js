6e90c5c092c0360a292fb551492274a78d962309
Luis Romero <dev@luisromero.co>
2023-11-21 00:46:42 -0500

    build application shell

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,5 +4,21 @@ module.exports = {
     './app/helpers/**/*.rb',
     './app/assets/stylesheets/**/*.css',
     './app/javascript/**/*.js'
-  ]
+  ],
+  theme: {
+    extend: {
+      gridTemplateRows: {
+        'layout': '64px 1fr'
+      },
+      gridTemplateColumns: {
+        'layout': '20% 1fr'
+      },
+      colors: {
+        'jw-black': 'hsl(0, 0%, 0%)',
+        'jw-bokara-gray': 'hsl(0, 0%, 16%)',
+        'jw-bunting': 'hsl(222, 64%, 17%)',
+        'jw-onyx': 'hsl(0, 0%, 7%)'
+      }
+    }
+  }
 }
