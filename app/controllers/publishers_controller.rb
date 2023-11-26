1ad42cfd5c126872d91892beaf91418cb5edf2f6
Luis Romero <dev@luisromero.co>
2023-11-26 18:46:51 -0500

    add catalog of publishers by group

--- app/controllers/publishers_controller.rb
+++ app/controllers/publishers_controller.rb
@@ -1,4 +1,6 @@
 class PublishersController < ApplicationController
   def index
+    @field_service_group_id = params[:field_service_group_id]
+    @publishers = Publisher.where(field_service_group_id: @field_service_group_id)
   end
 end
