237ce2eebcce99b6b93d2155198e49afff008584
Luis Romero <dev@luisromero.co>
2023-12-08 13:41:56 -0500

    tweak design

--- tailwind.config.js
+++ tailwind.config.js
@@ -15,11 +15,13 @@ module.exports = {
       },
       colors: {
         'jw-black': 'hsl(0, 0%, 0%)',
+        'jw-white': 'hsl(0, 0%, 100%)',
         'jw-bokara-gray': 'hsl(0, 0%, 16%)',
         'jw-bunting': 'hsl(222, 64%, 17%)',
         'jw-onyx': 'hsl(0, 0%, 7%)',
         'jw-san-marino': 'hsl(220, 33%, 48%)',
-        'jw-quick-silver': 'hsl(0, 0%, 65%)'
+        'jw-quick-silver': 'hsl(0, 0%, 65%)',
+        'jw-seashell': 'hsl(0, 0%, 95%)'
       }
     }
   },
