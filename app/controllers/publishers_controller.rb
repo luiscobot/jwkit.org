6a636d0efa24e98e9b3795d4d9953dd7f6ca862c
Luis Romero <dev@luisromero.co>
2023-12-08 20:13:26 -0500

    fix access to field service group number

--- app/controllers/publishers_controller.rb
+++ app/controllers/publishers_controller.rb
@@ -1,6 +1,6 @@
 class PublishersController < ApplicationController
   def index
-    @field_service_group_id = params[:field_service_group_id]
-    @publishers = Publisher.where(field_service_group_id: @field_service_group_id)
+    @field_service_group = FieldServiceGroup.find(params[:field_service_group_id])
+    @publishers = Publisher.where(field_service_group_id: @field_service_group.id)
   end
 end
