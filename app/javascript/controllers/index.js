452015ba0943cc518a8589b9cb66ca553c9a74ff
Luis Romero <dev@luisromero.co>
2023-12-13 01:12:19 -0500

    dismiss notification for successful creation of field service report after a period of time

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -4,8 +4,8 @@
 
 import { application } from "./application"
 
-import HelloController from "./hello_controller"
-application.register("hello", HelloController)
+import FlashController from "./flash_controller"
+application.register("flash", FlashController)
 
 import ToggleController from "./toggle_controller"
 application.register("toggle", ToggleController)
