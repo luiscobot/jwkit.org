041b403b4a5c25c5fe203c0cc82d236de740d744
Luis Romero <dev@luisromero.co>
2023-12-14 21:27:52 -0500

    include sidebar field service group links in field service report creation

--- app/controllers/field_service_reports_controller.rb
+++ app/controllers/field_service_reports_controller.rb
@@ -6,6 +6,7 @@ class FieldServiceReportsController < ApplicationController
 
   def new
     @publisher = Publisher.find(params[:publisher_id])
+    @field_service_group = @publisher.field_service_group
     @field_service_report = FieldServiceReport.new
   end
 
