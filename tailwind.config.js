ce518f4d15c4caff271cc7def28b37145ce83397
Luis Romero <dev@luisromero.co>
2024-03-08 01:00:30 -0500

    change custom font family

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,32 +1,35 @@
-module.exports = {
-  content: [
-    './app/views/**/*.html.erb',
-    './app/helpers/**/*.rb',
-    './app/assets/stylesheets/**/*.css',
-    './app/javascript/**/*.js'
-  ],
-  theme: {
-    extend: {
-      gridTemplateRows: {
-        'layout': '64px 1fr'
-      },
-      gridTemplateColumns: {
-        'layout': '20% 1fr'
-      },
-      colors: {
-        'jw-black': 'hsl(0, 0%, 0%)',
-        'jw-white': 'hsl(0, 0%, 100%)',
-        'jw-bokara-gray': 'hsl(0, 0%, 16%)',
-        'jw-bunting': 'hsl(222, 64%, 17%)',
-        'jw-onyx': 'hsl(0, 0%, 7%)',
-        'jw-san-marino': 'hsl(220, 33%, 48%)',
-        'jw-quick-silver': 'hsl(0, 0%, 65%)',
-        'jw-seashell': 'hsl(0, 0%, 95%)',
-        'success': 'hsl(121, 35%, 51%)'
-      }
-    }
+export const content = [
+  './app/views/**/*.html.erb',
+  './app/helpers/**/*.rb',
+  './app/assets/stylesheets/**/*.css',
+  './app/javascript/**/*.js'
+];
+
+export const theme = {
+  fontFamily: {
+    'latina-essential': ['"Latina Essential"', 'sans-serif']
   },
-  plugins: [
-    require('@tailwindcss/forms')
-  ],
-}
+  extend: {
+    gridTemplateRows: {
+      'layout': '64px 1fr'
+    },
+    gridTemplateColumns: {
+      'layout': '20% 1fr'
+    },
+    colors: {
+      'jw-black': 'hsl(0, 0%, 0%)',
+      'jw-white': 'hsl(0, 0%, 100%)',
+      'jw-bokara-gray': 'hsl(0, 0%, 16%)',
+      'jw-bunting': 'hsl(222, 64%, 17%)',
+      'jw-onyx': 'hsl(0, 0%, 7%)',
+      'jw-san-marino': 'hsl(220, 33%, 48%)',
+      'jw-quick-silver': 'hsl(0, 0%, 65%)',
+      'jw-seashell': 'hsl(0, 0%, 95%)',
+      'success': 'hsl(121, 35%, 51%)'
+    }
+  }
+};
+
+export const plugins = [
+  require('@tailwindcss/forms')
+];
