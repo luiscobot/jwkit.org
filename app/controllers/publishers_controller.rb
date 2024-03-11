9dd1b58fcee9fd15367897fc240331501492b0a9
Luis Romero <dev@luisromero.co>
2024-03-10 22:28:39 -0500

    tweak index publishers design

--- app/controllers/publishers_controller.rb
+++ app/controllers/publishers_controller.rb
@@ -5,6 +5,6 @@ class PublishersController < ApplicationController
 
   def index
     @field_service_group = FieldServiceGroup.find(params[:field_service_group_id])
-    @publishers = @field_service_group.publishers.missing_last_month_reports
+    @publishers = @field_service_group.publishers
   end
 end
