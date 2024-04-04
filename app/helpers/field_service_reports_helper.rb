009dfdc9c159eafcedf2bcd824846754c801f058
Luis Romero <dev@luisromero.co>
2024-04-04 01:21:21 -0500

    delete unused field service reports helper methods

--- app/helpers/field_service_reports_helper.rb
+++ app/helpers/field_service_reports_helper.rb
@@ -1,17 +1,2 @@
 module FieldServiceReportsHelper
-  def last_month
-    Date.current.last_month.month
-  end
-
-  def month_options(selected_option = nil)
-    options_for_select((1..12).map { |month| [Date::MONTHNAMES[month], month] }, selected_option || last_month)
-  end
-
-  def last_month_year
-    Date.current.last_month.year
-  end
-
-  def year_options(selected_option = nil)
-    options_for_select((last_month_year - 1..last_month_year + 1).to_a, selected_option || last_month_year)
-  end
 end
--- config/initializers/month_names.rb
+++
@@ -1,3 +0,0 @@
-if I18n.locale == :es
-  Date::MONTHNAMES = [nil, "Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"]
-end
