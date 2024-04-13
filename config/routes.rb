1bb8f2a102e7ce84680b55e806f955e87512ec24
Luis Romero <dev@luisromero.co>
2024-04-13 00:56:48 -0500

    connect debugbar gem

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,10 @@ Rails.application.routes.draw do
 
   passwordless_for :users
 
+  if defined? Debugbar
+    mount Debugbar::Engine => Debugbar.config.prefix
+  end
+
   resources :field_service_groups, only: :index, path: "groups" do
     resources :publishers, only: :index do
       resources :field_service_reports, only: [:new, :create], path: "reports"
