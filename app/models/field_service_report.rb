bcd736c545e97e33c43217f80367f30c1c3dd97e
Luis Romero <dev@luisromero.co>
2024-04-27 12:43:14 -0500

    add field service report from service year scope

--- app/models/field_service_report.rb
+++ app/models/field_service_report.rb
@@ -3,7 +3,6 @@ class FieldServiceReport < ApplicationRecord
 
   validates :month, uniqueness: { scope: [:publisher_id, :year] }
 
-  scope :from_last_month, -> {
-    where(year: Date.current.last_month.year, month: Date.current.last_month.month)
-  }
+  scope :from_last_month, -> { where(year: Date.current.last_month.year, month: Date.current.last_month.month) }
+  scope :from_service_year, ->(year = Date.current.year) { where(year: year) }
 end
