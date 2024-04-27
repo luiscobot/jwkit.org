3ebc71f2e627c5fdce981ee525676b9c00c7d947
Luis Romero <dev@luisromero.co>
2024-04-27 12:45:51 -0500

    update flash stimulus

--- app/javascript/controllers/flash_controller.js
+++ app/javascript/controllers/flash_controller.js
@@ -2,7 +2,7 @@ import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
   connect() {
-    setTimeout(() => {
+    setTimeout(_ => {
       this.dismiss();
     }, 3000);
   }
