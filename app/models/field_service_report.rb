b307fe29795c77c69757ec4191f671234057c7b1
Luis Romero <dev@luisromero.co>
2024-05-05 17:48:56 -0500

    update field service report model

--- app/models/field_service_report.rb
+++ app/models/field_service_report.rb
@@ -1,7 +1,7 @@
 class FieldServiceReport < ApplicationRecord
-  belongs_to :publisher
-
   validates :month, uniqueness: { scope: [:publisher_id, :year] }
+
+  belongs_to :publisher
 
   scope :from_last_month, -> { where(year: Date.current.last_month.year, month: Date.current.last_month.month) }
   scope :from_service_year, ->(year = Date.current.year) { where(year: year) }
