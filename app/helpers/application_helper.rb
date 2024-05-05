0358e22b8aa5cb0f5016baf0de6c21be07f522eb
Luis Romero <dev@luisromero.co>
2024-05-05 17:46:56 -0500

    tweak design

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,16 +1,4 @@
 module ApplicationHelper
-  def label_style
-    "font-inter font-medium text-base text-jw-onyx"
-  end
-
-  def checkbox_style
-    "h-6 w-6 rounded-lg border-none ring-2 ring-inset ring-jw-san-marino/50 text-jw-san-marino hover:ring-jw-san-marino focus:ring-inset focus:ring-jw-san-marino focus:ring-offset-0"
-  end
-
-  def control_style
-    "px-3 py-2 border-none rounded-lg font-inter text-base text-jw-onyx ring-2 ring-inset ring-jw-san-marino/50 hover:ring-jw-san-marino focus:ring-inset focus:ring-2 focus:ring-jw-san-marino disabled:bg-jw-san-marino/50 disabled:cursor-not-allowed disabled:ring-transparent"
-  end
-
   def reports_date(month, year)
     I18n.localize(Date.new(year.to_i, month.to_i, 1), format: "%B, %Y")
   end
