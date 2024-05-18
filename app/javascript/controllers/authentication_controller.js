005f575297fcdb2eed02d0abf5104316fa088bfd
Luis Romero <dev@luisromero.co>
2024-05-18 00:44:11 -0500

    validate email for authentication

--- app/javascript/controllers/authentication_controller.js
+++ app/javascript/controllers/authentication_controller.js
@@ -2,15 +2,53 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="authentication"
 export default class extends Controller {
-  static targets = ["otp"];
+  static targets = ["email", "otp"];
 
   connect() {
-    this.inputs = Array.from(this.otpTarget.querySelectorAll("[data-control='otp']"));
+    if (this.hasOtpTarget) {
+      this.inputs = Array.from(this.otpTarget.querySelectorAll("[data-control='otp']"));
+    }
+
+    if (this.hasEmailTarget) {
+      this.input = this.emailTarget.querySelector("[data-control='email']");
+    }
   }
 
   validate(ev) {
-    if (!/^[0-9a-zA-Z]{1}$/.test(ev.key) && ev.key !== "Tab" && ev.key !== "Backspace" && ev.key !== "Delete" && ev.key !== "Enter") {
-      ev.preventDefault();
+    if (this.hasOtpTarget) {
+      if (!/^[0-9a-zA-Z]{1}$/.test(ev.key) && ev.key !== "Tab" && ev.key !== "Backspace" && ev.key !== "Delete" && ev.key !== "Enter") {
+        ev.preventDefault();
+      }
+    }
+
+    if (this.hasEmailTarget) {
+      let error = this.emailTarget.querySelector("[data-authentication-error]");
+
+      if (!this.input.value.trim()) {
+        this.input.focus();
+        !this.emailTarget.contains(error) && this.invalidate("Ingresa un correo electrónico para iniciar sesión.");
+      } else if (!/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(this.input.value)) {
+        this.input.focus();
+        !this.emailTarget.contains(error) && this.invalidate("El correo electrónico no es válido.");
+      } else {
+        return true;
+      }
+    }
+  }
+
+  invalidate(message) {
+    this.input.classList.add("invalid");
+    this.input.classList.remove("ring-jw-bokara-gray", "hover:ring-jw-san-marino", "focus:ring-jw-san-marino");
+    this.input.insertAdjacentHTML("afterend", `<div data-authentication-error class="flex flex-col mt-2"><span class="font-inter font-medium text-sm text-jw-error">${message}</span></div>`);
+  }
+
+  clean() {
+    let error = this.emailTarget.querySelector("[data-authentication-error]");
+
+    if (this.emailTarget.contains(error)) {
+      this.input.classList.remove("invalid");
+      this.input.classList.add("ring-jw-bokara-gray", "hover:ring-jw-san-marino", "focus:ring-jw-san-marino");
+      error.remove();
     }
   }
 
@@ -53,11 +91,18 @@ export default class extends Controller {
   
   authenticate(ev) {
     ev.preventDefault();
-    let tokenControl = document.querySelector("input[name='passwordless[token]']");
-    let token = Array.prototype.map.call(this.inputs, function(input) {
-      return input.value.toUpperCase();
-    }).join("");
-    tokenControl.value = token;
-    ev.target.submit();
+
+    if (this.hasOtpTarget) {
+      let tokenControl = document.querySelector("input[name='passwordless[token]']");
+      let token = Array.prototype.map.call(this.inputs, function(input) {
+        return input.value.toUpperCase();
+      }).join("");
+      tokenControl.value = token;
+      ev.target.submit();
+    }
+
+    if (this.hasEmailTarget) {
+      this.validate() && ev.target.submit();
+    }
   }
 }
