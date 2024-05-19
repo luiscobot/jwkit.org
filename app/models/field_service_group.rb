5bf8e32f1b9c42bb4cca76f4aeffb220e5127c4d
Luis Romero <dev@luisromero.co>
2024-05-19 17:17:14 -0500

    add server side field service group validation

--- app/models/field_service_group.rb
+++ app/models/field_service_group.rb
@@ -1,5 +1,5 @@
 class FieldServiceGroup < ApplicationRecord
-  validates :number, presence: true, numericality: { only_integer: true }, uniqueness: true
+  validates :number, presence: { message: "Ingresa un número." }, numericality: { only_integer: true, greater_than_or_equal_to: 0, message: "Debe ser un número positivo." }, uniqueness: { message: "Ya existe un grupo con ese número." }
 
   has_many :publishers
   has_many :field_service_reports, through: :publishers
