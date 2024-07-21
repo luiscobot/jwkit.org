b6a8104e4ce4d07c33bc0a5c4e929e8b2cc42378
Luis Romero <dev@luisromero.co>
2024-07-21 15:54:59 -0500

    update rails 7.1.3.4

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,6 @@
-require_relative "boot"
+require_relative 'boot'
 
-require "rails/all"
+require 'rails/all'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -8,16 +8,13 @@ Bundler.require(*Rails.groups)
 
 module Jwkit
   class Application < Rails::Application
-    config.autoload_paths << "#{root}/app/views"
-    config.autoload_paths << "#{root}/app/views/layouts"
-    config.autoload_paths << "#{root}/app/views/components"
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
 
     # Please, add to the `ignore` list any other `lib` subdirectories that do
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
     # Common ones are `templates`, `generators`, or `middleware`, for example.
-    config.autoload_lib(ignore: %w(assets tasks))
+    config.autoload_lib(ignore: %w[assets tasks])
 
     # Configuration for the application, engines, and railties goes here.
     #
@@ -28,7 +25,7 @@ module Jwkit
     # config.eager_load_paths << Rails.root.join("extras")
 
     # Permitted locales available for the application
-    I18n.available_locales = [:en, :es]
+    I18n.available_locales = %i[en es]
 
     # Set default locale to something other than :en
     I18n.default_locale = :es
