cb6cc01d43cb9ac25aac23e4c272034cfdec44db
Luis Romero <dev@luisromero.co>
2024-11-14 19:01:56 -0500

    convert to JS Modules

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,34 +1,35 @@
-const defaultTheme = require('tailwindcss/defaultTheme');
+import defaultTheme from "tailwindcss/defaultTheme";
+import forms from "@tailwindcss/forms";
 
-export const content = [
-  './app/views/**/*.html.erb',
-  './app/helpers/**/*.rb',
-  './app/assets/stylesheets/**/*.css',
-  './app/javascript/**/*.js'
-];
-
-export const theme = {
-  fontFamily: {
-    'inter': ['Inter', ...defaultTheme.fontFamily.sans]
+const config = {
+  content: [
+    "./app/views/**/*.html.erb",
+    "./app/helpers/**/*.rb",
+    "./app/assets/stylesheets/**/*.css",
+    "./app/javascript/**/*.js",
+  ],
+  theme: {
+    fontFamily: {
+      inter: ["Inter", ...defaultTheme.fontFamily.sans],
+    },
+    extend: {
+      colors: {
+        "jw-black": "hsl(0, 0%, 0%)",
+        "jw-white": "hsl(0, 0%, 100%)",
+        "jw-bokara-gray": "hsl(0, 0%, 16%)",
+        "jw-bunting": "hsl(222, 64%, 17%)",
+        "jw-onyx": "hsl(0, 0%, 7%)",
+        "jw-san-marino": "hsl(220, 33%, 48%)",
+        "jw-quick-silver": "hsl(0, 0%, 65%)",
+        "jw-seashell": "hsl(0, 0%, 95%)",
+        "jw-success": "hsl(146, 63%, 41%)",
+        "jw-warning": "hsl(42, 100%, 60%)",
+        "jw-error": "hsl(345, 90%, 60%)",
+        "jw-info": "hsl(200, 100%, 50%)",
+      },
+    },
   },
-  extend: {
-    colors: {
-      'jw-black': 'hsl(0, 0%, 0%)',
-      'jw-white': 'hsl(0, 0%, 100%)',
-      'jw-bokara-gray': 'hsl(0, 0%, 16%)',
-      'jw-bunting': 'hsl(222, 64%, 17%)',
-      'jw-onyx': 'hsl(0, 0%, 7%)',
-      'jw-san-marino': 'hsl(220, 33%, 48%)',
-      'jw-quick-silver': 'hsl(0, 0%, 65%)',
-      'jw-seashell': 'hsl(0, 0%, 95%)',
-      'jw-success': 'hsl(146, 63%, 41%)',
-      'jw-warning': 'hsl(42, 100%, 60%)',
-      'jw-error': 'hsl(345, 90%, 60%)',
-      'jw-info': 'hsl(200, 100%, 50%)'
-    }
-  }
+  plugins: [forms],
 };
 
-export const plugins = [
-  require('@tailwindcss/forms')
-];
+export default config;
